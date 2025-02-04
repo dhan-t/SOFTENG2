8af8265e032b8565496b8f43cec337c08eec3a03
Dhan Tamparong <dhantcm@gmail.com>
2025-02-04 14:27:29 +0800

    test push

---
+++ app-analog-softeng2/frontend/src/components/Layout.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+import "./Layout.css";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const location = useLocation();
+  const noNavbarRoutes = ["/", "/register", "/login"];
+  const [isExpanded, setIsExpanded] = useState(true); // Navbar state
+
+  const isNoNavbarRoute = noNavbarRoutes.includes(location.pathname);
+
+  return (
+    <div
+      className={`layout ${
+        !isNoNavbarRoute && isExpanded
+          ? "expanded"
+          : !isNoNavbarRoute
+          ? "collapsed"
+          : ""
+      }`}
+    >
+      {!isNoNavbarRoute && (
+        <Navbar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+      )}
+      <main className="content">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ app-analog-softeng2/frontend/src/components/Navbar.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "./logos/image 1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt, faBars } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
+
+interface NavbarProps {
+  isExpanded: boolean;
+  setIsExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ isExpanded, setIsExpanded }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/");
+  };
+
+  return (
+    <nav className={`navbar ${isExpanded ? "expanded" : "collapsed"}`}>
+      <div className="navbar-top">
+        <div
+          className="logo-container"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <img src={String(logo)} alt="Logo" className="logo" />
+          {isExpanded && <h1 className="logo-text">Analog</h1>}
+        </div>
+      </div>
+
+      <ul className="nav-links">
+        <NavbarItem
+          text="Dashboard"
+          link="/dashboard"
+          isExpanded={isExpanded}
+          icon="📊"
+        />
+        <NavbarItem
+          text="Production Reports"
+          link="/production"
+          isExpanded={isExpanded}
+          icon="🏭 "
+        />
+        <NavbarItem
+          text="Module requests	"
+          link="/logistics"
+          isExpanded={isExpanded}
+          icon="📦"
+        />
+        <NavbarItem
+          text="Logistics tracking"
+          link="/tracking"
+          isExpanded={isExpanded}
+          icon="🚛"
+        />
+        <NavbarItem
+          text="Report Generation"
+          link="/reports"
+          isExpanded={isExpanded}
+          icon="📑"
+        />
+      </ul>
+
+      <div className="navbar-bottom">
+        <div className="user-info">
+          <img
+            src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+            alt="User Avatar"
+            className="user-avatar"
+          />
+          {isExpanded && (
+            <div className="user-details">
+              <h4>John Doe</h4>
+              <span>johndoe@gmail.com</span>
+            </div>
+          )}
+        </div>
+        <button className="logout-btn" onClick={handleLogout}>
+          {isExpanded ? "Logout" : <FontAwesomeIcon icon={faSignOutAlt} />}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+interface NavbarItemProps {
+  text: string;
+  link: string;
+  isExpanded: boolean;
+  icon: string;
+}
+
+const NavbarItem: React.FC<NavbarItemProps> = ({
+  text,
+  link,
+  isExpanded,
+  icon,
+}) => {
+  return (
+    <li className="nav-item">
+      <Link to={link} className="nav-link">
+        <span className="nav-icon">{icon}</span>
+        {isExpanded && <span className="nav-text">{text}</span>}
+      </Link>
+    </li>
+  );
+};
+
+export default Navbar;
